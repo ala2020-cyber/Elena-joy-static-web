f7ec6f8d61a178d232c0eb860e81b6de288fd255
Debabi Ala <73254102+ala2020-cyber@users.noreply.github.com>
2022-02-23 20:16:04 +0100

    commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+
+
+var bar=document.getElementById("bar-icon");
+var links=document.querySelector(".nav-links");
+
+bar.addEventListener("click", function (){
+    links.style.display="block";
+});
