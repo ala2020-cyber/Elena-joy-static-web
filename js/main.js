849777b7a0c795f77382a99aba3acb101dad5c87
Debabi Ala <73254102+ala2020-cyber@users.noreply.github.com>
2022-02-24 22:32:41 +0100

    responsive navbar

--- js/main.js
+++ js/main.js
@@ -3,7 +3,12 @@
 
 var bar=document.getElementById("bar-icon");
 var links=document.querySelector(".nav-links");
+var main=document.querySelector("main");
 
 bar.addEventListener("click", function (){
-    links.style.display="block";
+    links.style.display="flex";
+    links.classList.toggle("nav-links-open");
+});
+main .addEventListener("click", function () {
+    links.classList.remove("nav-links-open")
 });
