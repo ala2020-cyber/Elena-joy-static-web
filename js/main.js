dac6211e888fbbca1252176bbeafeb258f6c6639
Debabi Ala <73254102+ala2020-cyber@users.noreply.github.com>
2022-02-25 17:49:02 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 
 
 
+// navigation button functionalitie
 var bar=document.getElementById("bar-icon");
 var links=document.querySelector(".nav-links");
 var main=document.querySelector("main");
 
+
 bar.addEventListener("click", function (){
     links.style.display="flex";
     links.classList.toggle("nav-links-open");
+    bar.classList.toggle("fa-window-close");
 });
 main .addEventListener("click", function () {
-    links.classList.remove("nav-links-open")
+    links.classList.remove("nav-links-open");
+    bar.classList.remove("fa-window-close");
 });
