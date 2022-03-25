e23f7965e6c2b1465a10c5598a1782ced3f4a33f
Debabi Ala <73254102+ala2020-cyber@users.noreply.github.com>
2022-03-26 00:28:07 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -13,3 +13,14 @@ main .addEventListener("click", function () {
     links.classList.remove("nav-links-open");
     bar.classList.remove("fa-window-close");
 });
+
+
+var subject="Get in touch email";
+var body="Welcome Elena, I would like to be my photographer, contact me please.";
+var email=document.getElementById("email");
+
+
+function sendEmail(){
+    window.location.href="mailto:mainAccount@gmail.com?subject="+subject
+    +"&body="+body+"&cc="+email.value;
+}
